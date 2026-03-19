0694e5e9e1e64a24b1b1d9c3bc2ed8223dca8951
Vee <vjirapon@openware.com>
2026-03-19 19:48:21 +0700

    feat: Create WelcomeSection component

---
+++ src/components/dashboard/WelcomeSection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../styles/components/dashboard/_welcomeSection.scss';
+
+const WelcomeSection = () => {
+  return (
+    <div className="welcome-section">
+      <h1>Welcome to Trader Board!</h1>
+      <p>Your personal trading journal and analysis tool.</p>
+    </div>
+  );
+};
+
+export default WelcomeSection;
