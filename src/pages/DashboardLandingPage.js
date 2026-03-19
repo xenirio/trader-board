5abf5f1ca77ac54842cc0bcbbe8c23ccc59e0418
Vee <vjirapon@openware.com>
2026-03-19 16:07:22 +0700

    feat: Integrate Header into DashboardLandingPage

--- src/pages/DashboardLandingPage.js
+++ src/pages/DashboardLandingPage.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import WelcomeSection from '../components/WelcomeSection/WelcomeSection';
+import Header from '../components/Header/Header';
+import './DashboardLandingPage.css'; // Assuming some page-level styling
 
 const DashboardLandingPage = () => {
   return (
-    <div>
-      <WelcomeSection />
-      {/* Other dashboard content will go here */}
+    <div className="dashboard-landing-page">
+      <Header />
+      <main>
+        {/* Main content of the dashboard landing page */}
+        <h1>Welcome to Your Trader Board Dashboard!</h1>
+        <p>This is where your financial insights and tools will be displayed.</p>
+      </main>
     </div>
   );
 };
