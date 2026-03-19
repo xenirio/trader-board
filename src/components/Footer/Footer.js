1519e60c5f1b0511f3fbcb73bb8213c6d8505e3d
Vee <vjirapon@openware.com>
2026-03-19 16:02:03 +0700

    feat: Implement Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className={styles.footer}>
+      <p>&copy; {currentYear} Trader Board</p>
+      <p>Version: 1.0.0-beta</p>
+    </footer>
+  );
+};
+
+export default Footer;
