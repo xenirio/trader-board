a607be122917d1bbffb9dc94075a1695be8387c7
Vee <vjirapon@openware.com>
2026-03-19 19:43:28 +0700

    feat: Integrate Header component into App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header'; // Import Header component
+
+function App() {
+  return (
+    <div className="App">
+      <Header /> {/* Render Header component */}
+      {/* Rest of your App content goes here */}
+    </div>
+  );
+}
+
+export default App;
