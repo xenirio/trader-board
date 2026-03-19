a61a65ef14410cb5d3e59d3eef2d3cd69384c8a7
Vee <vjirapon@openware.com>
2026-03-19 16:07:31 +0700

    feat: Create Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-title">Trader Board</div>
+      <nav className="header-nav">
+        {/* Navigation links will go here */}
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
