e47dfbc93ac72523acb3503c5107cc13fc5e6034
Vee <vjirapon@openware.com>
2026-03-19 19:56:20 +0700

    feat: Integrate Header Component into Dashboard

---
+++ src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+
+function Dashboard() {
+  return (
+    <div>
+      <Header />
+      <h2>Dashboard Content</h2>
+      {/* Further dashboard content will go here */}
+    </div>
+  );
+}
+
+export default Dashboard;
