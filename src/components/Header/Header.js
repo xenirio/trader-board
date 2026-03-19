2363fdd0ae8be699810ec0744fddb21d8195a4dd
Vee <vjirapon@openware.com>
2026-03-19 19:44:11 +0700

    feat: Implement Header component structure

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,8 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="header">
-      <div className="header-title">Trader Board</div>
-      <nav className="header-nav">
-        {/* Navigation links will go here */}
-      </nav>
+      <h1>Trader Board</h1>
+      <nav></nav>
     </header>
   );
 };
