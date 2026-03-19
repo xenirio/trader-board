80568ddf32c76854b6f51e2a956afb57ae74ee45
Vee <vjirapon@openware.com>
2026-03-19 19:42:43 +0700

    feat: Implement Header component structure

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Trader Board</h1>
+      <nav></nav>
+    </header>
+  );
+};
+
+export default Header;
