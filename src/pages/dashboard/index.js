98fc2c629165ff6ad64b2192e82e7804d0a27ed7
Vee <vjirapon@openware.com>
2026-03-19 19:48:44 +0700

    feat: Integrate WelcomeSection into Dashboard landing page

---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WelcomeSection from '../../components/dashboard/WelcomeSection';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <WelcomeSection />
+      {/* Other dashboard content will go here */}
+    </div>
+  );
+};
+
+export default Dashboard;
