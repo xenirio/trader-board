a17f7630a33b88429f4110cbbe73ebdc94cf7846
Vee <vjirapon@openware.com>
2026-03-21 00:16:26 +0700

    feat: Integrate MarketOverviewHero into Dashboard page

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MarketOverviewHero from '../../components/MarketOverviewHero/MarketOverviewHero';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <MarketOverviewHero />
+      {/* Other dashboard content will go here */}
+      <h1>Dashboard</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
