1c0e9fca0de9c5cbc924ad0e023c6b31440946f0
Vee <vjirapon@openware.com>
2026-03-20 21:13:02 +0700

    feat: Integrate WelcomeSection into LandingPage

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import WelcomeSection from '../components/WelcomeSection';
+
+const LandingPage = () => {
+  return (
+    <div>
+      {/* Assuming there might be a Header component or similar here */}
+      <WelcomeSection />
+      {/* Assuming there might be a Footer component or similar here */}
+    </div>
+  );
+};
+
+export default LandingPage;
