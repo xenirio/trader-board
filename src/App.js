640945c9c0c5da9cbf8a8c3f96b2178678ec4027
Vee <vjirapon@openware.com>
2026-03-20 23:39:37 +0700

    feat: Integrate Footer into App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import Header from './components/Header';
+import './App.css';
+import Footer from './components/Footer/Footer'; // Import the Footer component
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      {/* Other components will go here */}
+      {/* Other components and content of your App */}
+      <main style={{ flex: 1 }}>
+        <h1>Welcome to Trader Board</h1>
+        <p>Your trading dashboard</p>
+      </main>
+      <Footer /> {/* Integrate the Footer component */}
     </div>
   );
 }
