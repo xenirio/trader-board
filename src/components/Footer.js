54f225de10765503ca6e335d898305be06f24499
Vee <vjirapon@openware.com>
2026-03-20 21:14:23 +0700

    feat: Implement Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,14 @@
+
 import React from 'react';
-import '../styles/Footer.css';
+import './Footer.css';
 
 const Footer = () => {
-  return (
-    <footer className="footer">
-      <p>© 2023 Trader Board</p>
-      <p>Version 1.0.0</p>
-    </footer>
-  );
+    return (
+        <footer className="footer">
+            <p>&copy; 2023 Trader Board</p>
+            <p>v1.0.0</p>
+        </footer>
+    );
 };
 
 export default Footer;
