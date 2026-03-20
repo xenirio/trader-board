90422058b8ca5b6420b73c4d92bc5c54a4e61da4
Vee <vjirapon@openware.com>
2026-03-20 22:33:02 +0700

    feat: Create DashboardLandingPage component

---
+++ src/components/DashboardLandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function DashboardLandingPage() {
+  return (
+    <html>
+      <head>
+        <title>Trader Board Dashboard</title>
+      </head>
+      <body>
+        {/* Dashboard content will go here */}
+      </body>
+    </html>
+  );
+}
+
+export default DashboardLandingPage;
