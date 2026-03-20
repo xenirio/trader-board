a73dde367f71dbec087886c438b1eac2bffb574e
Vee <vjirapon@openware.com>
2026-03-21 00:28:59 +0700

    chore: reset repo to clean state

--- src/data/mockAssetData.js
+++
@@ -1,7 +0,0 @@
-const assetData = [
-  { name: 'Crypto', percentage: 45, color: '#FF6384' },
-  { name: 'Stocks', percentage: 35, color: '#36A2EB' },
-  { name: 'Cash', percentage: 20, color: '#FFCE56' },
-];
-
-export default assetData;
--- src/templates/dashboard.html
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Dashboard</title>
-</head>
-<body>
-    <h1>Welcome to the Dashboard</h1>
-</body>
-</html>
