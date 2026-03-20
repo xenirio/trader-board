800dcf1417d63082b1510f3fd2c7eb352f77216f
Vee <vjirapon@openware.com>
2026-03-20 10:57:01 +0700

    feat: Integrate Header into DashboardLandingPage

--- src/pages/DashboardLandingPage/DashboardLandingPage.js
+++ src/pages/DashboardLandingPage/DashboardLandingPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import WelcomeSection from '../../components/WelcomeSection/WelcomeSection';
+import Header from '../../components/Header/Header';
 
 const DashboardLandingPage = () => {
   return (
     <div>
-      <WelcomeSection />
-      {/* Other dashboard content will go here */}
+      <Header />
+      <h1>Welcome to the Dashboard</h1>
+      {/* Other content for the dashboard landing page */}
     </div>
   );
 };
