e93552f93fe78a9ab4ea1fb073c640140ccfb2a7
Vee <vjirapon@openware.com>
2026-03-20 21:15:15 +0700

    feat: Implement Footer Component for Landing Page (#84)
    * feat: Implement Footer component
    
    * style: Add basic styling for Footer component
    
    * feat: Integrate Footer component into LandingPage

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,14 +1,16 @@
+
 import React from 'react';
-import WelcomeSection from '../components/WelcomeSection';
+import Footer from '../components/Footer'; // Assuming Footer is in src/components/Footer.js
 
 const LandingPage = () => {
-  return (
-    <div>
-      {/* Assuming there might be a Header component or similar here */}
-      <WelcomeSection />
-      {/* Assuming there might be a Footer component or similar here */}
-    </div>
-  );
+    return (
+        <div>
+            {/* Other LandingPage content would go here */}
+            <h1>Welcome to Trader Board</h1>
+            <p>Your ultimate platform for trading.</p>
+            <Footer />
+        </div>
+    );
 };
 
 export default LandingPage;
