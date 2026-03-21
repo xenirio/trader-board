fb7a360c17e5ead9d2ba9204018201febaa8c3b5
Vee <vee.jirapong@gmail.com>
2026-03-21 18:57:57 +0700

    feat: Convert FeatureCard to TypeScript

---
+++ app/components/FeatureCard.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from '../styles/Home.module.css';
+
+interface FeatureCardProps {
+  icon: React.ReactNode;
+  title: string;
+  description: string;
+}
+
+const FeatureCard: React.FC<FeatureCardProps> = ({ icon, title, description }) => {
+  return (
+    <div className={styles.featureCard}>
+      <div className={styles.featureIcon}>{icon}</div>
+      <h3 className={styles.featureTitle}>{title}</h3>
+      <p className={styles.featureDescription}>{description}</p>
+    </div>
+  );
+};
+
+export default FeatureCard;
