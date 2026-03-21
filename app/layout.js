fb2d9d9c8592d36e49ef049c8821bfd00b108477
Vee <vee.jirapong@gmail.com>
2026-03-21 22:20:01 +0700

    feat: create basic app layout

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import './globals.css';
 
 export const metadata = {
   title: 'Trader Board',
-  description: 'Welcome to Trader Board',
+  description: 'Welcome to the future of trading.',
 };
 
 export default function RootLayout({ children }) {
