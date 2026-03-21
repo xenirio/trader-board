b55e6abf919054d0bbe6fcced320a0af8398a93f
Vee <vee.jirapong@gmail.com>
2026-03-21 17:35:54 +0700

    feat: Implement Home Page Structure (Hero, Features, Footer) (#230)
    * feat: Add Home.module.css for styling home page components
    
    * feat: Add Hero component
    
    * feat: Add FeatureCard component
    
    * feat: Add Features component
    
    * feat: Add Footer component
    
    * feat: Integrate Hero, Features, and Footer into Home page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,16 @@
+import React from 'react';
+import Hero from './components/Hero';
+import Features from './components/Features';
+import Footer from './components/Footer';
+
 export default function Home() {
   return (
-    <main>
-      <h1>Welcome to Next.js!</h1>
-    </main>
+    <div>
+      <main>
+        <Hero />
+        <Features />
+      </main>
+      <Footer />
+    </div>
   );
 }
