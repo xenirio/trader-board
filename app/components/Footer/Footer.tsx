9ca03ad721ee1d222572ad91eb276c04a648d0f8
Vee <vee.jirapong@gmail.com>
2026-03-23 16:20:18 +0700

    feat: create Footer component and apply text class

---
+++ app/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.text}>© 2024 Trader Board. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
