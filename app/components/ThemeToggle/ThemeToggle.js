a80d98abb12823d837e245630e48f9e81fe7ce93
Vee <vee.jirapong@gmail.com>
2026-03-24 06:27:37 +0700

    fix: update import path in ThemeToggle.js to use @/ alias

--- app/components/ThemeToggle/ThemeToggle.js
+++ app/components/ThemeToggle/ThemeToggle.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React from 'react';
-import { useTheme } from '../../context/ThemeContext';
+import { useTheme } from '@/app/context/ThemeContext';
 import styles from './ThemeToggle.module.css';
 
 const ThemeToggle = () => {
