94ef545add4a4f3bd00552892e056b623cb17f14
Vee <vee.jirapong@gmail.com>
2026-03-23 21:47:35 +0700

    feat: Create Header component and embed ThemeToggle

---
+++ app/components/Header/Header.js
@@ -0,0 +1,16 @@
+"use client";
+
+import React from 'react';
+import ThemeToggle from '@/app/components/ThemeToggle/ThemeToggle';
+import styles from './Header.module.css';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1>Trader Board</h1>
+      <ThemeToggle />
+    </header>
+  );
+};
+
+export default Header;
