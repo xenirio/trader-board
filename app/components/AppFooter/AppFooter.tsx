9e5a6f4aa984d905d7479fef9c476b0bb36209c3
Vee <vee.jirapong@gmail.com>
2026-03-25 15:40:12 +0700

    feat: Create AppFooter component to resolve casing conflict

---
+++ app/components/AppFooter/AppFooter.tsx
@@ -0,0 +1,11 @@
+import styles from './AppFooter.module.css';
+
+const AppFooter = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>Â© 2024 Trader Board. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default AppFooter;
