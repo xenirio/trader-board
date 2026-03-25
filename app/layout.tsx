874f2a2a6d7e82f56e151caddd52e174a4b1bdf0
Vee <vee.jirapong@gmail.com>
2026-03-25 15:34:36 +0700

    fix: Revert footer component import casing

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from './theme-context';
 import Header from './components/Header/Header';
-import Footer from './components/footer/Footer';
+import Footer from './components/Footer/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
