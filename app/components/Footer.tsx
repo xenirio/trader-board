5ff6f8b277052baf2d822218f169523a1f0312b8
Vee <vee.jirapong@gmail.com>
2026-03-25 14:08:37 +0700

    feat: create Footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer style={{ textAlign: 'center', padding: '1rem' }}>
+      <p>© 2024 Trader Board</p>
+    </footer>
+  );
+};
+
+export default Footer;
